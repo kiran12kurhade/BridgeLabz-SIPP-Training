9fc3de8f87e42c603c4f10e3a65ed4296e7c5271
kiran12kuhrade <Github@1999>
2025-07-23 12:34:04 +0530

    Day1_ClassAndObject

---
+++ OOPs/Day1_ClassAndObject/Hello.java
@@ -0,0 +1,8 @@
+package OOPs.Day1_ClassAndObject;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Object-Oriented Programming in Java.");
+    }
+}
