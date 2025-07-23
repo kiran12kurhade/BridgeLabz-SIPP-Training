1cdba44ff3e9a4b5728bbc79574ce35b6687dc7a
kiran12kuhrade <Github@1999>
2025-07-23 12:43:48 +0530

    LinkedList

---
+++ DSA/LinkedList/Hello.java
@@ -0,0 +1,8 @@
+package DSA.LinkedList;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Linked Lists in Data Structures and Algorithms.");   
+    }
+}
